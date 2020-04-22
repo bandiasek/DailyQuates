f13888c08b1e2feaff56de8f71ab692368b6e0b8
bandiasek <ondrejbrendza@gmail.com>
2020-04-22 11:18:19 +0200

    Basic layout

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Button, Image } from 'react-native';
 
 export default function App() {
+  /*----basic-data-storages----*/
+  const [showingQuote, setShowingQuote] = useState({autor:"Vaši vývojári", text:"Pre získanie citátu použi tlačidlo..."});
+  
+  /*----all-functions----------*/
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View /*this is another container wich is not full width large*/ >
+        {/*there will go name of the app, similiar to tittle*/}
+        <View /*This is going to be container for quotation and buttons*/>
+            {/*there is going to be component quotation*/}
+            <View /*this is button section with opacities*/ >
+              <Button 
+                title='Ziskaj citát'
+              />
+              <TouchableOpacity>
+                <Image source={require()} /*there should go setting icon*/ />
+                <Text /*temporary*/ >Nastavenia</Text>
+              </TouchableOpacity>
+            </View>
+        </View>
+      </View>
     </View>
   );
 }
