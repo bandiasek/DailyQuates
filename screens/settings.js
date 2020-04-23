0ecd9c9713e0e2d262e682c7f6c4f392b4e98672
mankop <mankop@spse-po.sk>
2020-04-23 20:15:46 +0200

    Buttons working

--- screens/settings.js
+++ screens/settings.js
@@ -2,16 +2,22 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { settingsStyle } from '../style/settingsStyle';
 export default function App() {
+  const [Language, SetLanguage] = useState('Slovak');
   return (
     <View>
       <View style={settingsStyle.container}>
         <Text style={settingsStyle.header}>Set Language</Text>
-        <View style={[settingsStyle.buttonSection, settingsStyle.parentOfContent, settingsStyle.container]}>
+        <View
+          style={[
+            settingsStyle.buttonSection,
+            settingsStyle.parentOfContent,
+            settingsStyle.container,
+          ]}>
           <View style={settingsStyle.button}>
-            <Button title={'English'} />
+            <Button title={'English'} onPress={() => SetLanguage('English')} />
           </View>
           <View style={settingsStyle.button}>
-            <Button title={'Slovak'} />
+            <Button title={'Slovak'} onPress={() => SetLanguage('Slovak')} />
           </View>
         </View>
       </View>
