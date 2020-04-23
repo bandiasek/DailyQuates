77b72461d0b8115538a36adb80c1e5583a33540a
mankop <mankop@spse-po.sk>
2020-04-23 19:50:14 +0200

    Settings buttons

--- screens/settings.js
+++ screens/settings.js
@@ -1,17 +1,20 @@
-import React,{ useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { settingsStyle } from '../style/settingsStyle';
 export default function App() {
   return (
-      <View style={styles.container}>
-          <Text>Settings</Text>
+    <View>
+      <View style={settingsStyle.container}>
+        <Text style={settingsStyle.header}>Set Language</Text>
+        <View style={[settingsStyle.buttonSection, settingsStyle.parentOfContent, settingsStyle.container]}>
+          <View style={settingsStyle.button}>
+            <Button title={'English'} />
+          </View>
+          <View style={settingsStyle.button}>
+            <Button title={'Slovak'} />
+          </View>
+        </View>
       </View>
-    );
-  }
-
-  const styles = StyleSheet.create({
-      container: {
-          padding:25,
-          backgroundColor: 'green'
-      }
-  });
+    </View>
+  );
+}
---
+++ style/settingsStyle.js
@@ -0,0 +1,38 @@
+import { StyleSheet } from 'react-native';
+
+export const settingsStyle = StyleSheet.create({
+    
+    container: {
+        flex: 1,
+        alignContent: 'center',
+      },
+      
+      parentOfContent: {
+        paddingTop:'5%',
+      },
+      
+      header: {
+        textAlign: "center",
+        fontSize: 30
+      },
+      
+      content: {
+        padding: '5%',
+        height: '100%'
+      },
+      
+      buttonSection: {
+        flex: 1,
+        justifyContent:"space-between",
+        alignItems: 'center',
+        flexDirection: 'row',
+
+      },
+      
+      button:{
+        width: '40%',
+        height: 40
+      }
+    
+    
+});
