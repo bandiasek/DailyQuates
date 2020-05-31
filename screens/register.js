03366a69f77567cb980f15f4f659e619ff282c65
bandiasek <ondrejbrendza@gmail.com>
2020-05-31 14:38:21 +0200

    added keyboard avoiding view

--- screens/register.js
+++ screens/register.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState} from 'react';
 import Feather from 'react-native-vector-icons/Feather';
-import { Text, View, AsyncStorage, TouchableOpacity, TextInput } from 'react-native';
+import { Text, View, AsyncStorage, TouchableOpacity, TextInput, KeyboardAvoidingView } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 import { registerStyle } from '../style/registerStyle';
 import Logo from '../components/logo';
@@ -9,14 +9,18 @@ import Logo from '../components/logo';
 export default function Register({navigation }) {
      /*----all-data---------------*/
     const [data, setData] = useState({
+        name:'',
         email:'',
         password:'',
+        confirmPassword:'',
         emailCorrect: false,
+        nameCorrect: false,
+        confirmPasswordCorrect: false,
         secureTextEntry: true,
     });
 
     /*----all-functions----------*/
-    const textInputChange = (value) => {
+    const emailInputChange = (value) => {
             if(value.includes('@')&&value.includes('.')&&!value.includes(' ')){
                 setData({
                     ...data,
@@ -32,7 +36,39 @@ export default function Register({navigation }) {
             } 
     }
 
-    const login = () => {
+    const nameInputChange = (value) => {
+        if(value!==''){
+            setData({
+                ...data,
+                nameCorrect: true,
+                name: value
+            });
+        }else{
+            setData({
+                ...data,
+                nameCorrect: false,
+                name: value
+            });
+        }    
+    }
+
+    const confirmPasswordInputChange = (value) => {
+        if(value===data.password){
+            setData({
+                ...data,
+                confirmPasswordCorrect: true,
+                confirmPassword: value
+            });
+        }else{
+            setData({
+                ...data,
+                confirmPasswordCorrect: false,
+                confirmPassword: value
+            });
+        }
+    }
+
+    const register = () => {
         if(data.emailCorrect){
             if(data.password!==''){
                 loginRemote();
@@ -44,7 +80,7 @@ export default function Register({navigation }) {
         }
     }
    
-    const loginRemote = () => {
+    const registerRemote = () => {
                 console.log('--------------------------');
                 console.log('starting fetch mathod --->');
                     fetch(
@@ -99,8 +135,10 @@ export default function Register({navigation }) {
     },[]);
     
   return (
-        <View style={registerStyle.container}>
-            
+        <KeyboardAvoidingView
+            behavior='padding'
+            style={registerStyle.container}   
+            >
             <Animatable.View 
                 style={registerStyle.header}
                     animation='bounceIn'
@@ -111,10 +149,11 @@ export default function Register({navigation }) {
             </Animatable.View>
             
             <Animatable.View 
-                style={registerStyle.footer}
+                    style={registerStyle.footer}
                     animation='fadeInUpBig'
             >
-                <Text style={registerStyle.heading}>LOGIN</Text>
+                <Text style={registerStyle.heading}>REGISTER</Text>
+                {/*Nickname Input*/}
                 <View style={registerStyle.inputSection}>
                     <Feather
                         name='user'
@@ -122,10 +161,30 @@ export default function Register({navigation }) {
                         style={registerStyle.icon}
                     />
                     <TextInput
+                        placeholder='Your Nickname'
+                        autoCapitalize='none'
+                        style={registerStyle.textInput}
+                        onChangeText={(value)=>{nameInputChange(value)}}
+                    />
+                        <Feather
+                            name={data.nameCorrect ? 'check-circle' : 'circle'}
+                            color={data.nameCorrect ? 'green' : 'grey'}
+                            size={20}
+                            style={registerStyle.icon}
+                        />
+                </View>
+                {/*Email Input*/}
+                <View style={registerStyle.inputSection}>
+                    <Feather
+                        name='mail'
+                        size={20}
+                        style={registerStyle.icon}
+                    />
+                    <TextInput
                         placeholder='Your Email'
                         autoCapitalize='none'
                         style={registerStyle.textInput}
-                        onChangeText={(value)=>{textInputChange(value)}}
+                        onChangeText={(value)=>{emailInputChange(value)}}
                     />
                         <Feather
                             name={data.emailCorrect ? 'check-circle' : 'circle'}
@@ -134,7 +193,7 @@ export default function Register({navigation }) {
                             style={registerStyle.icon}
                         />
                 </View>
-                
+                {/*Password Input*/}                
                 <View style={registerStyle.inputSection}>
                     <Feather
                         name='lock'
@@ -167,21 +226,44 @@ export default function Register({navigation }) {
                         />
                     </TouchableOpacity>
                 </View>
-               
+                {/*Re-enter password Input*/}
+                <View style={registerStyle.inputSection}>
+                    <Feather
+                        name='lock'
+                        size={20}
+                        style={registerStyle.icon}
+                        
+                    />
+                    <TextInput
+                        placeholder='Confirm Password'
+                        style={registerStyle.textInput}
+                        autoCapitalize='none'
+                        secureTextEntry={true}
+                        onChangeText={(value)=>{confirmPasswordInputChange(value)}}
+                            />
+                        <Feather
+                            name={data.confirmPasswordCorrect ? 'check-circle' : 'circle'}
+                            color={data.confirmPasswordCorrect ? 'green' : 'grey'}
+                            size={20}
+                            style={registerStyle.icon,{paddingRight:12}}
+                        />
+                </View>
+                {/*Button section Input*/}
                 <View style={registerStyle.btnSection}>
                     <TouchableOpacity 
                         style={registerStyle.signInOpacity}
                         onPress={()=>{login()}}
                     >
-                        <Text style={registerStyle.signInText}>Sign In</Text>
+                        <Text style={registerStyle.signInText}>Sign Up</Text>
                     </TouchableOpacity>
 
                     <TouchableOpacity style={registerStyle.signUpOpacity}>
-                        <Text style={registerStyle.signUpText}>Don't have an account ?</Text>
+                        <Text style={registerStyle.signUpText}>Already registered ?</Text>
                     </TouchableOpacity>
                 </View>
 
             </Animatable.View>
-        </View>
+            
+        </KeyboardAvoidingView>
   );
 }
