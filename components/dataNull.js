ded34592fbee21ee67034f89938d1b37d8c5fdc9
bandiasek <ondrejbrendza@gmail.com>
2020-06-03 10:28:41 +0200

    Aded data null component

---
+++ components/dataNull.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function DataNull (){
+ 
+        return(
+            <View style={{
+                    flex:1, justifyContent:"center", alignContent:"center",alignItems:"center"
+                  }}>
+                  <Text style={{fontSize:30}}>
+                      Data null
+                    </Text>
+                </View>
+        );    
+}
